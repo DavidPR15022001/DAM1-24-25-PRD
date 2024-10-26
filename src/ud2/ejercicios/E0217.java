904a130486884ab23dcd1e39e4a1b17e227a2cf6
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-10-26 16:08:58 +0200

    ejercicios

--- src/ud2/ejercicios/E0217.java
+++ src/ud2/ejercicios/E0217.java
@@ -1,26 +1,28 @@
 package ud2.ejercicios;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class E0217 {
     public static void main(String[] args) {
-        int MAX = 99;
-        int MIN = 1;
-        Math.random();
-        double n = Math.random() * MAX;
-        int n2 = (int) (Math.random() * MAX);
-        int n3 = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
-
-        for (int i = 0; i < 100000; i++) {
-            int num = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
-            System.out.println(num);
-            if (num > MAX)
-                System.out.println("Número demasiado alto");
-            if (num < MIN)
-                System.out.println("Número demasiado bajo");
-            if (num == 1)
-                System.out.println("UNOOOO");
-            if (num == 99)
-                System.out.println("NOVENTA Y NUEVE");
-
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        
+        int numero1 = random.nextInt(99) + 1;
+        int numero2 = random.nextInt(99) + 1;
+        
+        System.out.println("¿Cuánto es " + numero1 + " + " + numero2 + "?");
+        
+        int respuestaUsuario = scanner.nextInt();
+        
+        int sumaCorrecta = numero1 + numero2;
+        
+        if (respuestaUsuario == sumaCorrecta) {
+            System.out.println("¡Correcto!");
+        } else {
+            System.out.println("Incorrecto. La respuesta correcta es " + sumaCorrecta);
         }
+
+        scanner.close();
     }
 }
