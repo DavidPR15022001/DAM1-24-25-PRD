99b15af0667a10e9ab5c57a82b20317d980e455e
David <davidpadinruibal@gmail.com>
2024-11-07 11:33:03 +0100

    util con llamadas

--- src/ud2/Util.java
+++ src/ud2/Util.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Util {
 
-    static boolean esPar(int n) {
+    public static boolean esPar(int n) {
         return n % 2 == 0;
     }
 
-    static boolean esBisiesto(int anho) {
+    public static boolean esBisiesto(int anho) {
         return anho % 400 == 0 || (anho % 4 == 0 && anho % 100 != 0);
     }
 
@@ -18,13 +18,32 @@ public class Util {
 
     static String notaEnTexto(int nota) {
         String notaEnTexto = "";
+
+        switch (nota) {
+            case 0, 1, 2, 3, 4:
+                notaEnTexto = "insuficiente";
+                break;
+            case 5:
+                notaEnTexto = "suficiente";
+                break;
+            case 6:
+                notaEnTexto = "bien";
+                break;
+            case 7, 8:
+                notaEnTexto = "notable";
+                break;
+            case 9, 10:
+                notaEnTexto = "sobresaliente";
+                break;
+        }
+
         return notaEnTexto;
     }
 
     static String notaEnTexto(double nota) {
 
         String notaEnTexto = "";
-        
+
         if (nota >= 0 && nota < 5) {
             notaEnTexto = "insuficiente";
         } else if (nota >= 5 && nota < 6) {
@@ -33,23 +52,21 @@ public class Util {
             notaEnTexto = "bien";
         } else if (nota >= 7 && nota < 9) {
             notaEnTexto = "notable";
-        } else if (nota >= 9 && nota < 10) {
-            notaEnTexto = "sobresaliente";
+        } else if (nota >= 9 && nota <= 10) {
+            notaEnTexto = "sobresaliente ";
         }
-        
+
         return notaEnTexto;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nota: ");
-        double nota = sc.nextDouble();
-        System.out.println(notaEnTexto(nota));
-    }
+        double notaDouble = 6.7;
+        System.out.println(notaEnTexto((int) notaDouble));
+        int notaInt = 8;
+        System.out.println(notaEnTexto(notaInt));
 
-    /*public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Número / Año ");
+        System.out.print("Número / Anho: ");
         int n = sc.nextInt();
         sc.close();
 
@@ -63,8 +80,9 @@ public class Util {
         else
             System.out.println("NO es bisiesto");
 
-            System.out.println(mayor (4, 17));
-            System.out.println(mayor (8, 1));
-        }/* */
+        System.out.println(mayor(4, 17));
+        System.out.println(mayor(8, 1));
+
+    }
 
 }
