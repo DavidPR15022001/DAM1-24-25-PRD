f7a41a5bf9d34d3c7ebe2c053e43ab74a277203d
David <davidpadinruibal@gmail.com>
2024-11-07 11:13:37 +0100

    util

--- src/ud2/Util.java
+++ src/ud2/Util.java
@@ -16,9 +16,40 @@ public class Util {
         return a > b ? a : b;
     }
 
+    static String notaEnTexto(int nota) {
+        String notaEnTexto = "";
+        return notaEnTexto;
+    }
+
+    static String notaEnTexto(double nota) {
+
+        String notaEnTexto = "";
+        
+        if (nota >= 0 && nota < 5) {
+            notaEnTexto = "insuficiente";
+        } else if (nota >= 5 && nota < 6) {
+            notaEnTexto = "suficiente";
+        } else if (nota >= 6 && nota < 7) {
+            notaEnTexto = "bien";
+        } else if (nota >= 7 && nota < 9) {
+            notaEnTexto = "notable";
+        } else if (nota >= 9 && nota < 10) {
+            notaEnTexto = "sobresaliente";
+        }
+        
+        return notaEnTexto;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Número: ");
+        System.out.println("Nota: ");
+        double nota = sc.nextDouble();
+        System.out.println(notaEnTexto(nota));
+    }
+
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Número / Año ");
         int n = sc.nextInt();
         sc.close();
 
@@ -34,6 +65,6 @@ public class Util {
 
             System.out.println(mayor (4, 17));
             System.out.println(mayor (8, 1));
-        }
+        }/* */
 
 }
