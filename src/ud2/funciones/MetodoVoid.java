7f5d1bf6d2ef3a22a0070e8ca3d73dca8430d3ce
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-11-14 12:36:01 +0100

    void

--- src/ud2/funciones/MetodoVoid.java
+++ src/ud2/funciones/MetodoVoid.java
@@ -10,7 +10,10 @@ public class MetodoVoid {
         System.out.print("Introduce cadena de texto: ");
         cadena = sc.nextLine();
         sc.close();
-        cajaTexto(cadena); // llamada al método
+        cajaTexto(cadena); // llamada al método que crea un recuadro con #
+
+        cajaTexto(cadena, '+'); // llamada al método que crea un recuadro con otro caracter
+
     }
 
     // método que muestra un String rodeado por un borde
@@ -26,6 +29,23 @@ public class MetodoVoid {
         for (int i = 1; i <= n + 4; i++) { // borde de arriba
             System.out.print("#");
         }
+        System.out.println();
+    }
+
+    // método que muestra un String rodeado por un borde
+    public static void cajaTexto(String str, char ch) {
+        int n = str.length(); // longitud del String
+
+        imprimirLinea(n, ch);
+        System.out.println(ch + " " + str + " " + ch); // cadena con un borde en cada lado
+        imprimirLinea(n, ch);
+    }
+
+    public static void imprimirLinea(int n, char ch) {
+        for (int i = 1; i <= n + 4; i++) { // borde de arriba
+            System.out.print(ch);
+        }
+        System.out.println();
     }
 
 }
