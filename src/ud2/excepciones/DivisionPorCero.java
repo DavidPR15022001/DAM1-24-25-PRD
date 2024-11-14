e7b6a9f1904e5ccbbc7b1786f2434969fe98d0b3
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-11-14 12:27:05 +0100

    CAMBIOS

---
+++ src/ud2/excepciones/DivisionPorCero.java
@@ -0,0 +1,14 @@
+package ud2.excepciones;
+
+public class DivisionPorCero {
+    public static void main(String[] args) {
+        int a = 4, b = 0;
+        try {
+            System.out.println(a/b);
+            System.out.println("Division exitosa");
+        } catch (ArithmeticException e) {
+            System.out.println("ERROR: División por cero?");
+            System.out.println(e.toString());
+        }
+    }
+}
