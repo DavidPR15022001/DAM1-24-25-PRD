4382ebb131b7fc0cd26ce9facd94c6dac4f8049e
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-11-19 13:58:48 +0100

    supermercado

---
+++ src/ud2/practicas/Supermercado.java
@@ -0,0 +1,19 @@
+package ud2.practicas;
+
+import java.util.Scanner;
+
+public class Supermercado {
+    public static void main(String[] args) {
+        double precio;
+        int cantidad;
+        Scanner sc= new Scanner(System.in);
+        precio = sc.nextDouble();
+        System.out.printf("Introduce el precio del producto en euros %.2f", precio);
+        cantidad = sc.nextInt();
+        System.out.println("Introduce la cantidad de unidades del producto");
+
+        
+        
+        
+    }
+}
