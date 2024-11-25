6e11bb8ccd5e9bbc7d1c6039119fefe1407e8156
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-11-25 12:30:14 +0100

    CAMBIOS

--- src/ud3/apuntes/FechaMain.java
+++ src/ud3/apuntes/FechaMain.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 
 public class FechaMain {
     public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-Fecha fecha = new Fecha();
-int d, m, a;
-System.out.println("Introduce fecha: ");
-System.out.println("dia: ");
-d = sc.nextInt();
-System.out.println("mes: ");
-m = sc.nextInt();
-System.out.println("año: ");
-a = sc.nextInt();
-fecha.asignarFecha(d, m, a);
-sc.close();
-if (fecha.fechaCorrecta()) {
-System.out.println(fecha.getDia() + "-" + fecha.getMes()+ "-" + fecha.getAño());
-} else {
-System.out.println("Fecha no valida");
+        Scanner sc = new Scanner(System.in);
+        Fecha fecha = new Fecha();
+        int d, m, a;
+        System.out.println("Introduce fecha: ");
+        System.out.println("dia: ");
+        d = sc.nextInt();
+        System.out.println("mes: ");
+        m = sc.nextInt();
+        System.out.println("año: ");
+        a = sc.nextInt();
+        fecha.asignarFecha(d, m, a);
+        if (fecha.fechaCorrecta()) {
+            System.out.println(fecha.getDia() + "-" + fecha.getMes() + "-" + fecha.getAño());
+        } else {
+            System.out.println("Fecha no valida");
+        }
+    }
 }
-}
-}
-
--- src/ud3/apuntes/Persona.java
+++ src/ud3/apuntes/Persona.java
@@ -1,7 +1,7 @@
 package ud3.apuntes;
 
 public class Persona {
-    //Datos
+    // Datos
     private String nombre;
     private String apellido1;
     private String apellido2;
@@ -11,25 +11,27 @@ public class Persona {
 
     public static int contadorPersonas;
 
-    //Métodos
-    public void inicializar(String n, String a1, String a2, int e){
-    nombre = n;
-    apellido1 = a1;
-    apellido2 = a2;
-    edad = e;
-    }
-    public String nombreCompleto() {
-    return nombre + " " + apellido1 + " " + apellido2;
-    }
-    public boolean esMayorEdad() {
-    if (edad >= 18){
-    return true;
-    }else{
-    return false;
-    }
+    // Métodos
+    public void inicializar(String n, String a1, String a2, int e) {
+        nombre = n;
+        apellido1 = a1;
+        apellido2 = a2;
+        edad = e;
     }
 
-    public void cumplirAnhos(){
+    public String nombreCompleto() {
+        return nombre + " " + apellido1 + " " + apellido2;
+    }
+
+    public boolean esMayorEdad() {
+        if (edad >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void cumplirAnhos() {
         edad++;
     }
 
@@ -37,7 +39,7 @@ public class Persona {
         return edad;
     }
 
-    public static void incrementarContador(){
+    public static void incrementarContador() {
         contadorPersonas++;
     }
-    }
+}
