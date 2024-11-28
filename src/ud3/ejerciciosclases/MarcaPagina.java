6924d6aaee6dde57d1d8672000111e8747db8f29
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-11-28 13:46:45 +0100

    cambios

--- src/ud3/ejerciciosclases/MarcaPagina.java
+++ src/ud3/ejerciciosclases/MarcaPagina.java
@@ -3,16 +3,17 @@ package ud3.ejerciciosclases;
 public class MarcaPagina {
     int pagina;
     String libro;
-
+    
     public void incrementarPagina() {
         pagina++;
     }
-    
+
     public int getPagina() {
         return this.pagina;
     }
 
-    public void reiniciarLibro () {
+    public void reiniciarLibro() {
         pagina = 0;
     }
+    
 }
