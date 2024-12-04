41182a3d6eb52f8b2c98fdb59ed47eb7602616ee
DavidPR15022001 <davidpadinruibal@gmail.com>
2024-12-04 12:59:42 +0100

    cambio

--- src/ud3/ejerciciosclases/EP0715.java
+++ src/ud3/ejerciciosclases/EP0715.java
@@ -4,6 +4,11 @@ public class EP0715 {
     public static void main(String[] args) {
         Calendario fecha = new Calendario(2024, 12, 31);
         fecha.incrementarDia();
+        fecha.incrementarAño(-2025);
+        fecha.mostrar();
+        fecha.incrementarAño(1);
+        fecha.mostrar();
+
 
 
         System.out.println("FIN");
