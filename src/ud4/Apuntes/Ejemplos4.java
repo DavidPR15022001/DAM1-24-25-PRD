477c83d9aaf5faa1056fc78e62525bf4f2f29eac
DavidPR15022001 <davidpadinruibal@gmail.com>
2025-01-08 13:51:57 +0100

    cambios

---
+++ src/ud4/Apuntes/Ejemplos4.java
@@ -0,0 +1,22 @@
+package ud4.apuntes;
+
+import java.util.Arrays;
+
+public class Ejemplos4 {
+    public static void main(String[] args) {
+        int[] t = new int[100];
+
+        Arrays.fill(t,7);
+        Arrays.fill(t,50,60,5);
+        for(int i = 20; i < 30; i++)
+            t[i] =2;
+
+        int suma = 0;
+        for(int numero : t) 
+            suma += numero;
+            
+        System.out.println("Suma: " + suma);
+
+        System.out.println(Arrays.toString(t));
+    }
+}
