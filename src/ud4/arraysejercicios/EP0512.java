8eb713a286157fed35247e3871c1178453081613
DavidPR15022001 <davidpadinruibal@gmail.com>
2025-01-14 13:43:00 +0100

    cambios

--- src/ud4/arraysejercicios/EP0512.java
+++ src/ud4/arraysejercicios/EP0512.java
@@ -1,12 +1,36 @@
 package ud4.arraysejercicios;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class EP0512 {
     public static void desordenar(int t[]) {
-        int[] t1 = {5, 6, 4, 7, 8, 9};
-        Arrays.sort(t1);
+        Random rnd = new Random();
+        for (int i = 0; i < t.length; i++) {
+            int nuevaPosicion = rnd.nextInt(t.length);
+            if (i != nuevaPosicion) {
+                int aux = t[i];
+                t[i] = t[nuevaPosicion];
+                t[nuevaPosicion] = aux;
+            }
+        }
         
     }
 
+    
+    public static void main(String[] args) {
+        int[] array = arrayAleatorio(6,1,10);
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        desordenar(array);
+        System.out.println(Arrays.toString(array));
+    }
+    public static int[] arrayAleatorio(int n, int rndInicio, int rndFin) {
+        int[] t = new int[n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++) {
+            t[i] = rnd.nextInt(rndFin - rndInicio + 1) + rndInicio;
+        }
+        return t;
+    }
 }
