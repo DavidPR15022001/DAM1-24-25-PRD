a7184d34b8700b14fddaa04165dcfad999621bee
DavidPR15022001 <davidpadinruibal@gmail.com>
2025-01-16 12:10:47 +0100

    cambios

--- src/ud4/arraysejercicios/ConceptosProgramacion.java
+++ src/ud4/arraysejercicios/ConceptosProgramacion.java
@@ -1,8 +1,26 @@
 package ud4.arraysejercicios;
 
 public class ConceptosProgramacion {
-    
-    static String fraseAleatoria(String[] t) {
 
+    public static void main(String[] args) {
+
+        String conceptos[] = { "Algoritmo",
+                "Lenguaje de programación",
+                "Entorno de desarrollo",
+                "Compilación",
+                "Ejecución",
+                "Código fuente",
+                "Bytecode",
+                "Código objeto"
+        };
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(fraseAleatoria(conceptos));
+        }
     }
+
+    static String fraseAleatoria(String[] t) {
+        return (t[(int) (Math.random() * t.length)]);
+    }
+
 }
