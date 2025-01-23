aa3216d0471e1031896742c90ec8217e078e085b
DavidPR15022001 <davidpadinruibal@gmail.com>
2025-01-23 13:03:30 +0100

    cambios

--- src/ud4/stringsapuntes/E0601.java
+++ src/ud4/stringsapuntes/E0601.java
@@ -2,7 +2,7 @@ package ud4.stringsapuntes;
 
 public class E0601 {
     public static void main(String[] args) {
-        for (int i = 0; i < 512; i++) // máximo = 65536
+        for (int i = 0; i < 512; i++)
             System.out.println(i + " -> " + (char) i );
         
     }
