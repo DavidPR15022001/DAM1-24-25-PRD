245a3e70f3ec4d1ba0e7df25dfda48f2adc11a59
DavidPR15022001 <davidpadinruibal@gmail.com>
2025-02-14 14:04:40 +0100

    cambios

---
+++ src/ud5/clasesejercicios/E0710/personal/Mecanico.java
@@ -0,0 +1,27 @@
+package ud5.clasesejercicios.E0710.personal;
+
+import java.time.LocalDate;
+
+import ud5.clasesejercicios.E0710.maquinaria.Locomotora;
+
+public class Mecanico {
+    String nombre;
+    String telefono;
+    Especialidad especialidad;
+
+    public Mecanico(String nombre, String telefono, Especialidad especialidad) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (telefono=" + telefono + ", especialidad=" + especialidad + ")";
+    }
+
+    public void revisar(Locomotora locomotora) {
+        System.out.println("El/La mecánico/a " + nombre + " ha revisado" + especialidad + "de" + locomotora + " con fecha " + LocalDate.now());
+    }
+    
+}
